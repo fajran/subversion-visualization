f7e859f03405460d035e264b944122cca0cf6d3b
Fajran Iman Rusadi <fajran@gmail.com>
2015-06-13 23:26:09 +0200

    Split single and multiple log groups parsing

--- svnvis/logparser.go
+++ svnvis/logparser.go
@@ -82,46 +82,50 @@ func parseChange(line string) Change {
 	}
 }
 
-func parseLog(groups <-chan []string) <-chan *RevisionLog {
+func parseLogGroup(lines []string) *RevisionLog {
+	header := lines[0]
+	p := strings.Split(header, "|")
+
+	revision := parseInt(strings.Trim(p[0], " ")[1:])
+	author := strings.Trim(p[1], " ")
+	timestamp := strings.Trim(p[2], " ")
+	info := strings.Trim(p[3], " ")
+
+	changes := make([]Change, 0)
+	logLines := make([]string, 0)
+
+	phase := 0
+	for _, line := range lines[2:] {
+		if phase == 0 && len(strings.Trim(line, " ")) == 0 {
+			phase = 1
+
+		} else if phase == 0 {
+			change := parseChange(line)
+			changes = append(changes, change)
+
+		} else if phase == 1 {
+			logLines = append(logLines, line)
+		}
+	}
+
+	log := strings.Trim(strings.Join(logLines, "\n"), " \n")
+
+	return &RevisionLog{
+		Revision:  revision,
+		Author:    author,
+		Timestamp: timestamp,
+		Info:      info,
+		Changes:   changes,
+		Log:       log,
+	}
+}
+
+func parseLogGroups(groups <-chan []string) <-chan *RevisionLog {
 	c := make(chan *RevisionLog, 1)
 
 	go func() {
 		for lines := range groups {
-			header := lines[0]
-			p := strings.Split(header, "|")
-
-			revision := parseInt(strings.Trim(p[0], " ")[1:])
-			author := strings.Trim(p[1], " ")
-			timestamp := strings.Trim(p[2], " ")
-			info := strings.Trim(p[3], " ")
-
-			changes := make([]Change, 0)
-			logLines := make([]string, 0)
-
-			phase := 0
-			for _, line := range lines[2:] {
-				if phase == 0 && len(strings.Trim(line, " ")) == 0 {
-					phase = 1
-
-				} else if phase == 0 {
-					change := parseChange(line)
-					changes = append(changes, change)
-
-				} else if phase == 1 {
-					logLines = append(logLines, line)
-				}
-			}
-
-			log := strings.Trim(strings.Join(logLines, "\n"), " \n")
-
-			c <- &RevisionLog{
-				Revision:  revision,
-				Author:    author,
-				Timestamp: timestamp,
-				Info:      info,
-				Changes:   changes,
-				Log:       log,
-			}
+			c <- parseLogGroup(lines)
 		}
 		close(c)
 	}()
@@ -147,5 +151,5 @@ func readLines(r io.Reader) <-chan string {
 func ParseLog(r io.Reader) <-chan *RevisionLog {
 	lines := readLines(r)
 	groups := splitLogLines(lines)
-	return parseLog(groups)
+	return parseLogGroups(groups)
 }
