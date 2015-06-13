6e8069ff16d87c327b4dde6e43ccf0f0c4212c87
Fajran Iman Rusadi <fajran@gmail.com>
2015-06-13 21:45:16 +0200

    Fix import and variable

--- main.go
+++ main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	"svnvis"
+	"github.com/fajran/subversion-visualization/svnvis"
 )
 
 func main() {
 	p := svnvis.ParseLog(os.Stdin)
 	for revlog := range p {
-		fmt.Printf("%v\n", p)
+		fmt.Printf("%v\n", revlog)
 	}
 }
